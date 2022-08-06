6b940198704729fe74abded46d84cdef038c91e1
adityak994 <96017104+adityak994@users.noreply.github.com>
2022-08-06 15:45:11 +0530

    Update Thread.java

--- Thread.java
+++ Thread.java
@@ -1,12 +1,7 @@
 
 public class MyThread {
-    // Main method
     public static void main(String argvs[]) {
-        // creating an object of the Thread class using the constructor Thread(String
-        // name)
         Thread t = new Thread("My thread");
-
-        // the start() method moves the thread to the active state
         t.start();
         // getting the thread name by invoking the getName() method
         String str = t.getName();
