87a00c5969d111f2ea753b36246a750e38cbc7a5
adityak994 <96017104+adityak994@users.noreply.github.com>
2022-08-06 15:31:34 +0530

    Update Thread.java

--- Thread.java
+++ Thread.java
@@ -1,29 +1,14 @@
-import java.util.*;
-public class ThreadDemo1 extends Thread {
-    public void run()
-    {
-        // gets the name of current thread
-        System.out.println(
-            "Current Thread Name: "
-            + Thread.currentThread().getName());
+import java.io.FileNotFoundException;
+
+class Multi extends Thread {
+    public void run() {
+        System.out.println("thread is running...");
     }
-    public static void main(String[] args)
-        throws InterruptedException
-    {
-        Scanner s = new Scanner(System.in);
-        
-        // creating first thread
-        ThreadDemo1 t1 = new ThreadDemo1();
-        
-        // creating second thread
-        ThreadDemo1 t2 = new ThreadDemo1();
-        
-        // Starting the thread
+
+    public static void main(String args[]) {
+        Multi t1 = new Multi();
         t1.start();
-        t2.start();
-        
-        // t2 does not start execution until t1 completes
-        // execution
-        t1.join();
+        String str = t1.getName();
+        System.out.println(str);
     }
 }
